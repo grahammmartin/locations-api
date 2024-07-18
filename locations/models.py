1bd3da489a79877a05ce10173581d1bfb88a5447
grahammmartin <work.murtza2@gmail.com>
2024-07-18 04:01:11 -0700

    add `LocationRequest`

--- locations/models.py
+++ locations/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
+
+class LocationRequest(TimeStampedModel):
+    address = models.TextField()
+    formatted_address = models.TextField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["address"]),
+            models.Index(fields=["latitude", "longitude"]),
+        ]
+
+    def __str__(self):
+        return self.formatted_address or self.address
+
+    @property
+    def point(self):
+        return self.latitude, self.longitude
