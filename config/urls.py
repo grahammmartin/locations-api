aace1c18b62cea0141c4528ccde49e5ffb26a747
grahammmartin <work.murtza2@gmail.com>
2024-07-18 05:18:57 -0700

    add root path redirection

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="api/")),
     path("admin/", admin.site.urls),
     path("api/", include("locations.urls")),
 ]
